a77d29f8b719b09548d0f270519bf57068e3f884
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-10-13 21:24:35 -0300

    terminado a coleta de dados da api e estilizacao na tela

--- pokedex/src/app/pokemon-list/pokemon-list.component.ts
+++ pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,22 +1,16 @@
 import { Component } from '@angular/core';
-import { Pokemon } from '../../model/pokemon';
-import { Type } from '../../model/type';
+import { PokemonService } from '../_services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.scss']
+  styleUrls: ['./pokemon-list.component.sass'],
 })
 export class PokemonListComponent {
-  public pokemons: Pokemon[] = [
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      number: 1,
-      name: 'Bulbasaur',
-      types: [
-        Type.Grass,
-        Type.Poison
-      ]
-    }
-  ];
+
+  constructor(
+    public pokemonService: PokemonService,
+  ) {
+  }
 }
+
