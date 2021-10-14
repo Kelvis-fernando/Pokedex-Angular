31eca03f446449bb6c5fb6c1017e929ead8b2f6f
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-10-13 21:09:17 -0300

    Criado o module para guardar os tipos pokemon e types, e feito a logica com a diretiva ngFor para mostrar todos os pokemons dentro do array de objetos

--- pokedex/src/app/pokemon-card/pokemon-card.component.ts
+++ pokedex/src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/model/pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss']
 })
-export class PokemonCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonCardComponent {
+  @Input('pokemon')
+  public pokemon: Pokemon;
 }
--- pokedex/src/app/pokemon-list/pokemon-list.component.ts
+++ pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pokemon } from '../../model/pokemon';
+import { Type } from '../../model/type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonListComponent {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison
+      ]
+    }
+  ];
 }
